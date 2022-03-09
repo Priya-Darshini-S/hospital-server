b97d971f5925d71ac78da3e26ac474b274993fec
Priya_darshini_s <priya06200@gmail.com>
2022-03-09 13:18:14 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,21 @@
 const app = require("./index");
 const connect = require("./configs/db");
 const cors = require ("cors");
+
 app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    if (res.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "GET, POST, DELETE, PUT, PATCH"
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 const PORT = process.env.PORT || 5001;
 
